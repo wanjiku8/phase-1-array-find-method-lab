853cfa18e9e5a0c3066fbbefcc8b2bda9d52639f
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-05 22:32:23 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  console.log(superbowlWin(record)); // Output: "2015"
+
+
+  
